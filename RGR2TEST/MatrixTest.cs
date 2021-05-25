f4a7d404bc33e429ba6b18362c0023cc8b5bd1f8
DEHuckaKpyT <den-matytsin@mail.ru>
2021-05-25 21:34:24 +1000

    +test

--- RGR2TEST/MatrixTest.cs
+++ RGR2TEST/MatrixTest.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using RGR2;
 
-namespace RGR2TEST
+namespace RGR2Test
 {
-    class MatrixTest
+    [TestClass]
+    public class MatrixTest
     {
-        RGR2.Matrix.
+        [TestMethod]
+        public void MatrixMultiplication1x2and2x2Test1()
+        {
+            int[,] matrixA = new int[1, 2] { { 5, 6 } };
+            int[,] matrixB = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+
+            int[,] result = Matrix.MatrixMultiplication1x2and2x2(matrixA, matrixB);
+
+            CollectionAssert.AreEqual(result, new int[1, 2] { { 23, 34 } });
+            //Assert.AreEqual(result, new int[1, 2] { { 23, 34 } });
+        }
     }
 }
